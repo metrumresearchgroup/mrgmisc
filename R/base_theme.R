948a8ae8246a23a0392cefa73707f29f086de8b8
Devin Pastoor <devin.pastoor@gmail.com>
2014-03-22 00:54:48 -0400

    update base_theme to better defaults for knitting and add base_theme_obs with old defaults for interactive viewing in plot window

--- R/base_theme.R
+++ R/base_theme.R
@@ -2,7 +2,20 @@
 
 #' A theme with better default values for pharmacometric plots, especially conc-time
 #' @export 
-base_theme <- function() { theme(legend.text = element_text(size = 18),
+base_theme <- function() { theme(legend.text = element_text(size = 12),
+                                 legend.title = element_text(size = 14),
+                                 axis.title.x = element_text(size = 16, face = "bold"),
+                                 axis.title.y = element_text(size = 16, face = "bold"),
+                                 axis.text.x = element_text(color = "black", size = 12),
+                                 axis.text.y = element_text(color = "black", size = 12),
+                                 strip.text.x = element_text(color = "black", size = 10, face = "bold"))
+}
+
+#' A theme with better default values for pharmacometric plots, especially conc-time
+#' 
+#' made for viewing in the plot window and copying out
+#' @export 
+base_theme_obs <- function() { theme(legend.text = element_text(size = 18),
                                  legend.title = element_text(size = 20),
                                  axis.title.x = element_text(size = 22, face = "bold"),
                                  axis.title.y = element_text(size = 22, face = "bold"),
