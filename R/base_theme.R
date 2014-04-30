0697d3dfd5b704026aa81e81d1c64392ef0b5d60
dpastoor <devin.pastoor@gmail.com>
2014-04-30 01:29:56 -0400

    Update base_theme.R

--- R/base_theme.R
+++ R/base_theme.R
@@ -1,11 +1,10 @@
 
-
 #' A theme with better default values for pharmacometric plots, especially conc-time
 #' @export 
 base_theme <- function() { theme(legend.text = element_text(size = 10),
                                  legend.title = element_text(size = 12),
-                                 axis.title.x = element_text(size = 14, face = "bold"),
-                                 axis.title.y = element_text(size = 14, face = "bold"),
+                                 axis.title.x = element_text(size = 12, face = "bold"),
+                                 axis.title.y = element_text(size = 12, face = "bold"),
                                  axis.text.x = element_text(color = "black", size = 10),
                                  axis.text.y = element_text(color = "black", size = 10),
                                  strip.text.x = element_text(color = "black", size = 10, face = "bold"))
@@ -15,11 +14,11 @@ base_theme <- function() { theme(legend.text = element_text(size = 10),
 #' 
 #' made for viewing in the plot window and copying out
 #' @export 
-base_theme_obs <- function() { theme(legend.text = element_text(size = 18),
-                                 legend.title = element_text(size = 20),
-                                 axis.title.x = element_text(size = 22, face = "bold"),
-                                 axis.title.y = element_text(size = 22, face = "bold"),
-                                 axis.text.x = element_text(color = "black", size = 18),
-                                 axis.text.y = element_text(color = "black", size = 18),
-                                 strip.text.x = element_text(color = "black", size = 16, face = "bold"))
+base_theme_obs <- function() { theme(legend.text = element_text(size = 14),
+                                 legend.title = element_text(size = 16),
+                                 axis.title.x = element_text(size = 14, face = "bold"),
+                                 axis.title.y = element_text(size = 14, face = "bold"),
+                                 axis.text.x = element_text(color = "black", size = 12),
+                                 axis.text.y = element_text(color = "black", size = 12),
+                                 strip.text.x = element_text(color = "black", size = 14, face = "bold"))
 }
