1ab72dd4160e8937e0e4c435b24302bde031396a
Devin Pastoor <devin.pastoor@gmail.com>
2014-07-01 12:26:09 -0400

    add rename function from plyr for convenience

---
+++ R/rename.R
@@ -0,0 +1,22 @@
+#' Modify names by name, not position.
+#' @details 
+#' is simply the plyr rename function, but reduces need to load plyr 
+#' only for rename function to keep
+#' namespace clashes down between plyr and dplyr
+#' @param x named object to modify
+#' @param replace named character vector, with new names as values, and
+#'   old names as names.
+#' @param warn_missing print a message if any of the old names are
+#'   not actually present in \code{x}.
+#' Note: x is not altered: To save the result, you need to copy the returned
+#'   data into a variable.
+#' @export
+#' @importFrom plyr rename
+#' @examples
+#' x <- c("a" = 1, "b" = 2, d = 3, 4)
+#' # Rename column d to "c", updating the variable "x" with the result
+#' x <- rename(x, replace=c("d" = "c"))
+#' x
+#' # Rename column "disp" to "displacement"
+#' rename(mtcars, c("disp" = "displacement"))
+rename <- plyr::rename
