e29a0cf2be89c5feb7b7d520ea8a61db1da2c0ca
Devin Pastoor <devin.pastoor@gmail.com>
2014-12-12 13:01:17 -0500

    add stratification to resample_df and get ready to document

--- R/resampling_functions.R
+++ R/resampling_functions.R
@@ -1,27 +1,3 @@
-#' resampling
-#' @param df
-#' @param key_cols
-#' @param strat_cols
-#' @param key_name name of key column
-#' @param ... dots
-#' @export
-resample_df <- function(df, 
-                        key_cols, 
-                        strat_cols = NULL, 
-                        key_col_name = "KEY") {
-  names <- c(key_col_name,names(df))
-  key <- get_key(df, key_cols)
-  
-  
-  sample <- dplyr::sample_n(key, size = nrow(key), replace=T)
-  sample[[key_col_name]] <- 1:nrow(sample)
-  resampled_df <- dplyr::left_join(sample, df, by = key_cols)
-  
-  
-  #reorder columns to match original df with key column appended
-  return(resampled_df[,names, drop=F])
-}
-
 #' find unique values for key
 #' @param df data frame
 #' @param key_cols vector of column names
@@ -45,6 +21,39 @@ stratify_df <- function(df,
     dplyr::sample_frac(1, replace=T)
 }
 
+#' resampling
+#' @param df
+#' @param key_cols
+#' @param strat_cols
+#' @param key_name name of key column
+#' @param ... dots
+#' @export
+resample_df <- function(df, 
+                        key_cols, 
+                        strat_cols = NULL, 
+                        key_col_name = "KEY") {
+  names <- c(key_col_name,names(df))
+  key <- get_key(df, key_cols)
+  
+  if(is.null(strat_cols)) {
+    sample <- dplyr::sample_n(key, size = nrow(key), replace=T)
+    sample[[key_col_name]] <- 1:nrow(sample)
+  } else {
+    
+    key_strat <- dplyr::inner_join(key, 
+                                   df[, c(key_cols, strat_cols)])
+    sample <- stratify_df(key_strat, strat_cols)
+    #drop strat cols so won't possibly mangle later left join
+    sample <- sample[, key_cols, drop=F] 
+    sample[[key_col_name]] <- 1:nrow(sample)
+  }
+  
+  resampled_df <- dplyr::left_join(sample, df, by = key_cols)
+  
+  
+  #reorder columns to match original df with key column appended
+  return(resampled_df[,names, drop=F])
+}
 
 # tests
 # 
@@ -55,3 +64,13 @@ stratify_df <- function(df,
 # stratify_df(sid_dat, strat_cols="Gender")%>% summarize(n = n())
 # sid_dat %>% group_by(Gender, Race) %>% summarize(n = n())
 # stratify_df(sid_dat, strat_cols=c("Gender", "Race"))%>% summarize(n = n())
+
+# rep_dat <- rbind_all(lapply(1:5, function(x) dat %>%
+#                               filter(ID < 5) %>% 
+#                               mutate(REP = x)))
+# resample_df(rep_dat, key_cols = c("ID", "REP"))
+# rep_dat %>% group_by(Gender) %>% summarize(n = n())
+# stratify_df(rep_dat, strat_cols="Gender")%>% summarize(n = n())
+# rep_dat %>% group_by(Gender, Race) %>% summarize(n = n())
+# stratify_df(rep_dat, strat_cols=c("Gender", "Race"))%>% summarize(n = n())
+
