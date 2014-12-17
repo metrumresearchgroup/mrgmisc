6808341816ac85e0450aedf66340605c4770c4d6
Devin Pastoor <devin.pastoor@gmail.com>
2014-12-17 11:33:45 -0500

    remove import so builds properly, just dont want to export as is internal function

--- R/quantiles.R
+++ R/quantiles.R
@@ -3,18 +3,39 @@
 #' @param col_name string name for column to calculate quantiles on
 #' @param prob probability for quantiles of interest
 #' @param na.rm whether to remove na values
-#' @examples
-#' \dontrun{
-#' Theoph %>% s_quantile("conc", 0.1)
-#'}
-#' @export
-s_quantile <- function(df, col_name, prob, na.rm) {
+s_quantile <- function(df, col_name, prob, na.rm=T) {
   dots = list(lazyeval::interp(~ quantile(var, 
                                 prob, 
                                 na.rm=na.rm, 
                                 names = F,), 
                      var = as.name(col_name)))
-  df %>% dplyr::summarize_(.dots = setNames(dots, paste0("q",prob*10)))
+  df %>% dplyr::summarize_(.dots = setNames(dots, paste0("q",prob*100)))
+}
+
+#' summarize quantiles
+#' @param df data frame
+#' @param col_name string name for column to calculate quantiles on
+#' @param prob probability for quantiles of interest
+#' @param na.rm whether to remove na values
+#' @examples
+#' \dontrun{
+#' library(PKPDdatasets)
+#' sd_oral_richpk  %>% s_quantiles("Conc", probs = c(0.1, 0.5)) 
+#' sd_oral_richpk  %>% group_by(Gender) %>% s_quantiles("Conc", probs = c(0.1, 0.5)) 
+#'}
+#' @export
+s_quantiles <- function(df, col_name, probs, na.rm=T) {
+    quantiles <- lapply(probs, function(x) {
+    s_quantile(df, col_name, x, na.rm)
+    }
+    )
+    #check if grouped df and if so adjust behavior to bind together the list
+    # of quantiles given back from lapply
+    if(any(grepl("grouped", attributes(df)$class))) {
+      suppressMessages(do.call(dplyr::inner_join, quantiles)) }
+    else {
+      unlist(quantiles)
+    }
 }
 
 
