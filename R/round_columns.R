f13fdb646ecbd4d8cd25f71ba68a24fe3bbab2c9
Devin Pastoor <devin.pastoor@gmail.com>
2015-01-20 10:32:51 -0800

    add round columns but need to rewrite

---
+++ R/round_columns.R
@@ -0,0 +1,19 @@
+#' @examples
+#' \dontrun{
+#' library(PKPDdatasets)
+#' col_list <- list(list("AGE", 1), 
+#' list("WEIGHT", 2),
+#' list("SCREATININE", 2),
+#' list("SERUMALT", 2),
+#' list("DELDBP", 2),
+#' list("DELSBP", 2))
+#' round_columns(aht_trial2, col_list)
+#' }
+round_columns <- function(df, col_list) {
+  df <- df
+  for (i in seq_along(rounded)) {
+    df[[col_list[i][[1]][[1]]]] <- round(df[[col_list[i][[1]][[1]]]], 
+                                                col_list[i][[1]][[2]])
+  }
+  return(df)
+}
