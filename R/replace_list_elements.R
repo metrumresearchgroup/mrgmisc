cac7acab30d315f18b5e282d6ea223ff61a6988d
Devin Pastoor <devin.pastoor@gmail.com>
2015-01-21 17:35:16 -0800

    add replace_list_elements

---
+++ R/replace_list_elements.R
@@ -0,0 +1,22 @@
+#' replace list elements by name
+#' @param list original list
+#' @param replacement replacement list
+#' @details
+#' Finds and replaces list elements by name and throws an error if an 
+#'    element is not available in the original list
+#' @examples 
+#' \dontrun{
+#' list <- list(dv = "dv", idv = "idv")
+#' replacement <- list(dv = "conc", idv = "time")
+#' list <- replace_list_elements(list, replacement)
+#' }
+#' @export
+replace_list_elements <- function(list, replacement) {
+  missing <- which(!names(replacement) %in% names(list))
+  if(length(missing) != 0) {
+    warning(paste("Nothing named: ", paste(names(replacement)[missing], collapse= ", ", "found to replace") ))
+    replacement <- replacement[-missing]
+  }
+  list[names(replacement)] <- lapply(names(replacement), function(x) list[[x]] <- replacement[[x]])
+  return(list)
+}
