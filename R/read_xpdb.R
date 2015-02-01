e92385e32e339c5e4becaea870d6f6977f26054d
Devin Pastoor <devin.pastoor@gmail.com>
2015-01-31 23:13:07 -0500

    update read_xpdb

--- R/read_xpdb.R
+++ R/read_xpdb.R
@@ -1,7 +1,9 @@
 #' read xpose tables to xpose database and save and compress as RDS
 #' @param runno run number
-#' @param ... arguments to pass to xpose.data
+#' @param table.names table names for xpose.data to search for
+#' @param RDS_name name of RDS save file
 #' @param rm_tabs logical value to delete output tables after saving to xpdb
+#' @param ... arguments to pass to xpose.data
 #' @export
 #' @examples
 #' \dontrun{
@@ -10,11 +12,16 @@
 #' @return NULL
 read_xpdb <- function(runno,
                       table.names=c("sdtab","mutab","patab","catab",
-                                          "cotab","mytab","extra","xptab","cwtab"),
-                      ..., 
-                      RDS_name = paste0("xpdb",runno)) {
-  require(xpose4)
-  xpdb <- xpose.data(runno, table.names = table.names, ...)
+                                          "cotab","mytab","extra","xptab","cwtab"), 
+                      RDS_name = paste0("xpdb",runno),
+                      rm_tabs = FALSE,
+                      ...) {
+  
+  if(!requireNamespace("xpose4", quietly = TRUE)) {
+    stop("need xpose4 to use this function, please install!")
+  }
+  
+  xpdb <- xpose4::xpose.data(runno, table.names = table.names, ...)
   if(is.null(xpdb)) stop("Error creating xpose database, nothing created")
   RDS = paste0(RDS_name,".rds")
   saveRDS(xpdb, file=RDS)
