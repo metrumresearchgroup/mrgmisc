116b622e0350f7d621af80cea5e8f2fddf1fb0c8
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-04 17:29:49 -0500

    add rename_table_pattern

---
+++ R/rename_table_pattern.R
@@ -0,0 +1,23 @@
+#' simple table name replacement for a given pattern
+#' @param df dataframe to rename
+#' @param pattern pattern to replace
+#' @param replacement value to replace pattern
+#' @details
+#' a helper to easily wrap around reading tables to perform
+#' simple replacement
+#' @examples
+#' \dontrun{
+#' # rename all spaces to underscore
+#' df <- rename_table_spaces(read_table("data/data.csv"))
+#' # rename "-" to "_"
+#' df <- rename_table_spaces(read_table("data/data.csv"), "-", "_")
+#' #rename an existing df
+#' df <-rename_table_spaces(df, "-", "_")
+#' }
+#' @export
+rename_table_pattern <- function(df, pattern = " ", replacement = "_") {
+  names(df) <- gsub(pattern, 
+                    replacement = replacement, 
+                    x = names(df))
+  df
+}
