319b271440afb05c6089329d358942b5f31dcedd
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-10 16:33:07 -0500

    possibly a line endings difference?

--- R/base_theme.R
+++ R/base_theme.R
@@ -7,12 +7,12 @@
 #' @param axis_text_y size of Y axis text
 #' @param strip_text_x size of strip text for X axis
 #' @param strip_text_y size of strip text for Y axis
-#' @examples 
+#' @examples
 #' \dontrun{
 #' ggplot() + base_theme_obs()
 #' made for viewing in the plot window and copying out
 #' }
-#' @export 
+#' @export
 base_theme <- function(legend_text= 14,
                            legend_title = 16,
                            axis_title_x = 14,
@@ -21,13 +21,14 @@ base_theme <- function(legend_text= 14,
                            axis_text_y = axis_text_x,
                            strip_text_x = 16,
                            strip_text_y = strip_text_x
-                           ) { ggplot2::theme(legend.text = element_text(size = legend_text),
-                                 legend.title = element_text(size = legend_title),
-                                 axis.title.x = element_text(size = axis_title_x, face = "bold"),
-                                 axis.title.y = element_text(size = axis_title_y, face = "bold"),
-                                 axis.text.x = element_text(color = "black", size = axis_text_x),
-                                 axis.text.y = element_text(color = "black", size = axis_text_y),
-                                 strip.text.x = element_text(color = "black", size = strip_text_x, face = "bold"),
-                                 strip.text.y = element_text(color = "black", size = strip_text_y, face = "bold")
+                           ) {
+                           ggplot2::theme(legend.text = ggplot2::element_text(size = legend_text),
+                                 legend.title = ggplot2::element_text(size = legend_title),
+                                 axis.title.x = ggplot2::element_text(size = axis_title_x, face = "bold"),
+                                 axis.title.y = ggplot2::element_text(size = axis_title_y, face = "bold"),
+                                 axis.text.x = ggplot2::element_text(color = "black", size = axis_text_x),
+                                 axis.text.y = ggplot2::element_text(color = "black", size = axis_text_y),
+                                 strip.text.x = ggplot2::element_text(color = "black", size = strip_text_x, face = "bold"),
+                                 strip.text.y = ggplot2::element_text(color = "black", size = strip_text_y, face = "bold")
                                )
 }
