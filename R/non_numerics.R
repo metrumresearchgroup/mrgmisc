13243e35fe04cdb28397f88a08025cdacbaca88d
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-11 14:04:56 -0500

    new function for finding non numeric values

---
+++ R/non_numerics.R
@@ -0,0 +1,23 @@
+#' find all unique non-numeric values
+#' @param x vector to check on
+#' @param na.rm remove existing na values before checking
+#' @details
+#' This function is especially useful for figuring out what
+#' non-numeric unique values are in in a column that should be numeric
+#' so one can easily replace them with another flag. This function can work well
+#' with \code{replace_char_flags} instead of using nested ifelse statements
+#' @export
+#' @examples
+#' \dontrun{
+#' dv <- c(1, 2, 4, "88 (excluded)", "bql", "*")
+#' non_numerics(dv)
+#' replace_char_flags(dv, c("88 (excluded)" = -99, "bql" = -98, "*" = -97))
+#' flag <- replace_char_flags(dv, c("88 (excluded)" = -99, "bql" = -98, "*" = -97), 
+#'    nonflag = 0)
+#' flag
+#' }
+non_numerics <- function(x, na.rm = TRUE) {
+  if(na.rm) x <- x[!is.na(x)]
+  xn <- suppressWarnings(as_numeric(x))
+  unique(x[is.na(xn)])
+}
