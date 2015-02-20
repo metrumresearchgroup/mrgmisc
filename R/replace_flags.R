ca6b1c2d726421a500c9744f92e541c31e327174
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-19 22:57:56 -0500

    first shot to replace flag vector with a dataframe

--- R/replace_flags.R
+++ R/replace_flags.R
@@ -1,16 +1,16 @@
 #' replace symbols or other character flags
 #' @param x vector to replace
-#' @param flag vector with names being the flag and value the replacement
+#' @param flag dataframe with first column being the flag and second the replacement value
 #' @param nonflag what to do with non-flagged elements
 #' @export
 replace_char_flags <- function(x, 
-                         flag= c("*" = 1, "-" = 2),
+                         flag,
                          nonflag = NULL,
                          as_numeric=TRUE) {
-  if(!is.null(nonflag)) {x[!(x %in% names(flag))] <- nonflag}
-  tempx <- x[x %in% names(flag)]
-  matches <- match(tempx, names(flag))
-  x[x %in% names(flag)] <- unlist(lapply(matches, function(x) flag[x]))
+  if(!is.null(nonflag)) {x[!(x %in% flag[[1]])] <- nonflag}
+  tempx <- x[x %in% flag[[1]]]
+  matches <- match(tempx, flag[[1]])
+  x[x %in% flag[[1]]] <- unlist(lapply(matches, function(x) flag[[2]][x]))
   if(as_numeric) x <- as_numeric(x)
   x
 }
