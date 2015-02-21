c5971ad616fb5e3171fe5c30b93782159564c606
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-20 21:59:50 -0500

    add some replace flag tests

---
+++ tests/testthat/test-replacement-flags.R
@@ -0,0 +1,16 @@
+context("replacement-flags")
+
+
+
+vector <- c(1, 1, "*", "-", 5)    
+replacementn99 <- data.frame(flag = c("*", "-"), rep=c(-99))
+correct_outputn99 <- c(1, 1, -99, -99, 5)
+correct_outputn99_repNA <- c(NA, NA, -99, -99, NA)
+correct_outputn1n2 <- c(1, 1, -1, -2, 5)
+correct_output_wzero <- list(0, 0, 1, 2, 0)
+
+test_that("list elements replaced properly", {
+  expect_equal(replace_char_flags(vector, replacementn99), correct_outputn99),
+  expect_equal(replace_char_flags(vector, replacementn99,nonflag = NA), correct_outputn99_repNA)
+  
+})
