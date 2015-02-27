32cb91c416bb4a707eaa6281430ade2f053bdf7d
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-26 23:53:38 -0500

    add example and check for factors before doing replacements

--- R/replace_flags.R
+++ R/replace_flags.R
@@ -2,11 +2,19 @@
 #' @param x vector to replace
 #' @param flag dataframe with first column being the flag and second the replacement value
 #' @param nonflag what to do with non-flagged elements
+#' @examples 
+#' df <- data.frame(ID = 1, DV = c(1, "BQL", ".", 5))
+#' rflags <- data.frame(flag = c("BQL", "."), replacement = -99)
+#' df$DVR <- replace_char_flags(df$DV, rflags)
 #' @export
 replace_char_flags <- function(x, 
                          flag,
                          nonflag = NULL,
                          as_numeric=TRUE) {
+  if(is.factor(x)) {
+    message("converting factor to character strings before replacement")
+    x <- as.character(x)
+  }
   if(!is.null(nonflag)) {x[!(x %in% flag[[1]])] <- nonflag}
   tempx <- x[x %in% flag[[1]]]
   matches <- match(tempx, flag[[1]])
