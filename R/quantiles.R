e953093c7971834971bf8bcbf9131fa6711d38c4
Devin Pastoor <devin.pastoor@gmail.com>
2015-02-27 21:26:20 -0500

    update s_quantiles to handle lazy evaluation

--- R/quantiles.R
+++ R/quantiles.R
@@ -4,11 +4,12 @@
 #' @param prob probability for quantiles of interest
 #' @param na.rm whether to remove na values
 s_quantile <- function(df, col_name, prob, na.rm=T) {
+  col_name <- lazyeval::as.lazy(col_name)
   dots = list(lazyeval::interp(~ quantile(var, 
                                 prob, 
                                 na.rm=na.rm, 
                                 names = F), 
-                     var = as.name(col_name)))
+                     var = col_name$expr))
   df %>% dplyr::summarize_(.dots = setNames(dots, paste0("q",prob*100)))
 }
 
@@ -27,7 +28,8 @@ s_quantile <- function(df, col_name, prob, na.rm=T) {
 #' sd_oral_richpk  %>% group_by(Gender) %>% s_quantiles("Conc", probs = c(0.1, 0.5)) 
 #'}
 #' @export
-s_quantiles <- function(df, col_name, probs, na.rm=T) {
+#' @rdname s_quantiles
+s_quantiles_ <- function(df, col_name, probs=c(0, 0.25, 0.5, 0.75, 1), na.rm=T) {
     quantiles <- lapply(probs, function(x) {
     s_quantile(df, col_name, x, na.rm)
     }
@@ -46,6 +48,10 @@ s_quantiles <- function(df, col_name, probs, na.rm=T) {
     else {
       return(unlist(quantiles))
     }
+}
+s_quantiles <- function(df, col_name, probs=c(0, 0.25, 0.5, 0.75, 1), na.rm=T) {
+  col_name <- lazyeval::lazy(col_name)
+  s_quantiles_(df, col_name, probs, na.rm)
 }
 #sd_oral_richpk %>% group_by(Gender) %>% s_quantiles("Conc", probs = c(0.7))
 #sd_oral_richpk %>% group_by(Gender) %>% s_quantiles("Conc", probs = c(0.5, 0.7))
