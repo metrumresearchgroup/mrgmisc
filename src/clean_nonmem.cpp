95f8d6c6476557d56f8de8e76840cbc1f79cf0f2
Devin Pastoor <devin.pastoor@gmail.com>
2015-03-10 00:59:01 -0400

    remove unneeded boilerplate

--- src/clean_nonmem.cpp
+++ src/clean_nonmem.cpp
@@ -3,21 +3,6 @@
 #include <string>
 // [[Rcpp::depends(BH)]]
 using namespace Rcpp;
-
-// Below is a simple example of exporting a C++ function to R. You can
-// source this function into an R session using the Rcpp::sourceCpp 
-// function (or via the Source button on the editor toolbar)
-//
-// Learn more about how to use Rcpp at:
-//
-//   http://www.rcpp.org/
-//   http://adv-r.had.co.nz/Rcpp.html
-//
-// and browse examples of code using Rcpp at:
-// 
-//   http://gallery.rcpp.org/
-//
-
 // [[Rcpp::export]]
 std::string clean_nonmem(std::vector<std::string> x) {
   int n = x.size();
