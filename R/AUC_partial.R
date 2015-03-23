27e2d224a34ef72d0d1b0c1c1cc3064b89f290be
Devin Pastoor <devin.pastoor@gmail.com>
2015-03-23 17:13:40 -0400

    update api to idv/dv rather than time conc

--- R/AUC_partial.R
+++ R/AUC_partial.R
@@ -1,6 +1,6 @@
 #' Calculate partial AUC
-#' @param .time time column
-#' @param conc concentration column
+#' @param idv time column
+#' @param dv concentration column
 #' @param range time range for pauc calculation
 #' @details 
 #' default range is 0 to tmax
@@ -11,29 +11,34 @@
 #' individuals have.
 #' @examples
 #' \dontrun{
+#' library(PKPDdatasets)
+#' library(dplyr)
+#' df <- capitalize_names(sd_oral_richpk)
+#' df2 <- rename(sd_oral_richpk, idv = Time, dv = Conc)
 #' df %>% group_by(ID) %>% summarize(pAUC0_10 = auc_partial(TIME, DV, c(0,10)))
+#' df2 %>% group_by(ID) %>% summarize(auc0_tlast = auc_partial(idv, dv, c(0, Inf))) # gives auc 0 to tlast
 #' }
 #' @export
-auc_partial <-function(.time, 
-                       conc, 
-                       range = c(0, max(.time))
+auc_partial <-function(idv, 
+                       dv, 
+                       range = c(0, Inf)
                        ){
-  if(!is.numeric(.time) || !is.numeric(conc) || !is.numeric(range)) {
-    stop("time, conc/dv, and range inputs must all be numeric")
+  if(!is.numeric(idv) || !is.numeric(dv) || !is.numeric(range)) {
+    stop("time, dv/dv, and range inputs must all be numeric")
   }
   tfirst <- range[1]
   tlast <- range[2]
   
-  .time<- .time
-  conc <- conc[.time >= tfirst]
-  .time <- .time[.time >= tfirst]
-  if(length(.time) == 0) {
+  idv<- idv
+  dv <- dv[idv >= tfirst]
+  idv <- idv[idv >= tfirst]
+  if(length(idv) == 0) {
     warning("no observations in requested time range")
     return(setNames(NA, paste0("pAUC", tfirst, "-", tlast)))
   }
-  partial.time <- length(.time[.time <= tlast])
+  partial.time <- length(idv[idv <= tlast])
   
-  time.points <- length(.time)
+  time.points <- length(idv)
   #check to make sure partial time legit option
   ###need to add warning
   
@@ -41,12 +46,12 @@ auc_partial <-function(.time,
   
 
   for(i in 1:(partial.time-1)){
-    aucp[i]<-(conc[i]+conc[i+1])*(.time[i+1]-.time[i])/2
+    aucp[i]<-(dv[i]+dv[i+1])*(idv[i+1]-idv[i])/2
   }
   
   #calculate the starting part of AUC
   auc.start <-0
-  #if(time[1]!=0) auc.start <-time[1]*conc[1]/2 # need to check validity of this start calculation
+  #if(time[1]!=0) auc.start <-time[1]*dv[1]/2 # need to check validity of this start calculation
 
   AUC.partial <- sum(aucp) + auc.start
 
