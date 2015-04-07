c574fd52711a0aceff9222e6f79df99abcdd05de
Devin Pastoor <devin.pastoor@gmail.com>
2015-04-06 20:45:07 -0400

    add oridinal_to_binary_

--- R/ordinal_to_binary.R
+++ R/ordinal_to_binary.R
@@ -1,11 +1,28 @@
 #' convert a column of categorical covariates into a number of 
 #' columns with a binary flag for each category
+#' @param df data frame
+#' @param col_name column name to split into multiple binary flags
+#' @param prefix string name if want to specify a prefix to label 
+#'    the binary flag columns other than the original col_name
+#' @param overwrite overwrite any existing columns if the newly generated columns
+#'    share the same name
 #' @rdname oridinal_to_binary
+#' @examples \donttest{
+#' library(dplyr)
+#' df <- data_frame(OCC = c(1, 1, 2, 3))
+#' df %>% ordinal_to_binary_("OCC")
+#' df %>% ordinal_to_binary_("OCC", prefix = "OCCASION")
+#' 
+#' df2 <- data_frame(OCC = c(1, 1, 2, 3), OCC1 = 999)
+#' df2 %>% ordinal_to_binary_("OCC")
+#' df2 %>% ordinal_to_binary_("OCC", overwrite=T)
+#' }
+#' @export
 ordinal_to_binary_ <- function(df, 
                                col_name, 
                                id = NULL, 
                                prefix = NULL, 
-                               overwrite = F) {
+                               overwrite = FALSE) {
   if(!is.numeric(df[[col_name]])) {
     stop("Currently, column to convert must be numeric")
   }
@@ -17,9 +34,20 @@ ordinal_to_binary_ <- function(df,
   }, df, col_name)
   #df_cols <- do.call(dplyr::data_frame, cols)
   df_cols <- do.call(cbind, cols)
+  if(!is.null(prefix)) {
+    names(df_cols) <- stringr::str_replace(names(df_cols), col_name, prefix)
+  } 
   
+  # check if any column names exist in both
+  same_names <- dplyr::intersect(names(df_cols), names(df))
+  
+  if (overwrite) {
+    for (i in seq_along(same_names)) {
+      df[[same_names[i]]] <- NULL 
+    }
+  }
   #make sure no intersecting column names
-  if(length(setdiff(names(df_cols), names(df)) > 0 & !overwrite) {
+  if(length(same_names) > 0 & !overwrite) {
     warning("detected existing columns with same name sequence, 
             prepending \"_\" to all generated columns.")
     names(df_cols) <- paste0("_", names(df_cols))
@@ -27,6 +55,4 @@ ordinal_to_binary_ <- function(df,
   return(cbind(df, df_cols))
 }
 
-test_df <- data_frame(OCC = c(1, 1, 2, 3))
 
-ordinal_to_binary_(test_df, "OCC")
