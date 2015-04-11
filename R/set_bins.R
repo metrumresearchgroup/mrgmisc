ec03444ae5e8528f5fe020b05d2297283f71e290
Devin Pastoor <devin.pastoor@gmail.com>
2015-04-11 13:41:04 -0400

    update set_bins to also handle between

--- R/set_bins.R
+++ R/set_bins.R
@@ -4,6 +4,7 @@
 #' @param lower_bound set a lower bound for the first bin, defaults to -Inf
 #' @param upper_bound set an upper bound for the last bind, defaults to Inf
 #' @param quiet whether to give additonal information regarding bins and assigned range for each
+#' @param between defaults to NULL, a special case of setting all inside the specified range
 #' @details
 #' Given a set of quantiles/bins/etc established from a separate dataset, it can 
 #' be useful to assign the same bins to new or simulated data for comparisons
@@ -13,9 +14,32 @@
 #' If there is concern over data being outside the range of the assigned breaks, 
 #' one can assign -Inf to lower and/or Inf to upper to make sure all values will be 
 #' assigned to a bin
+#' 
+#' To use the between functionality, you must specify the range you wish to bin between,
+#' and those values will be assigned to bin 1, with all values below as 0 and all values
+#' above as 2. See the examples for more details
 #' @export
-set_bins <- function(x, breaks = quantile(x), lower_bound = -Inf, upper_bound = Inf, quiet = TRUE) {
+set_bins <- function(x, breaks = quantile(x), lower_bound = -Inf, upper_bound = Inf, quiet = TRUE,
+                     between = NULL ) {
   breaks <- breaks[order(breaks)]
+  
+  if (!is.null(between)) {
+    if(length(between) != 2) {
+      stop("can only have 2 breaks to use the between functionality")
+    }
+      x_bins <- ifelse(dplyr::between(x, between[1], between[2]), 1, 
+                       ifelse(x > between[2], 2, 0))
+      if(!quiet) {
+        message(paste0("there were 3 bins calculated, with the following
+                   range for each bin: "))
+        message(paste0("BIN 0: All values less than ", between[1]))
+        message(paste0("BIN 1: All values between ", between[1], " and ", between[2]))
+        message(paste0("BIN 2: All values greater than ", between[2]))
+      }
+      return(x_bins)
+  }
+  
+  
   if (breaks[1] > lower_bound && !is.null(lower_bound)) breaks <- c(-Inf, breaks)
   if (breaks[length(breaks)] < upper_bound && !is.null(upper_bound)) breaks <- c(breaks, Inf)
   if(length(breaks) ==1) breaks <- c(-Inf, breaks, Inf)
