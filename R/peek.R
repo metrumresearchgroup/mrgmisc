11646b03617bfede094b92afc655ca887aef5a82
Devin Pastoor <devin.pastoor@gmail.com>
2015-05-08 13:55:49 -0400

    have peek also show tail

--- R/peek.R
+++ R/peek.R
@@ -2,9 +2,9 @@
 #' @param df dataframe in pipeline
 #' @param n number of rows to show
 #' @details 
-#' A wrapper around giving a \code{head} command but only as a side effect
-#' so the original data frame is passed along to continue on for further
-#' manipulation in the pipeline
+#' A wrapper around giving a \code{head} and \code{tail} command
+#'  but only as a side effect so the original data frame is passed 
+#'  along to continue on for furthermanipulation in the pipeline
 #' @export
 #' @examples 
 #' library(dplyr)
@@ -14,6 +14,13 @@
 #' Theoph %>% select(Subject, Time, conc) %>% peek(message = "after select") %>% 
 #'  group_by(Subject) %>% 
 #' summarize(cmax = max(conc)) 
+#' 
+#' # nice for saving full objects but still seeing their output
+#' cmax_theoph <- Theoph %>% select(Subject, Time, conc) %>% 
+#' peek(message = "after select") %>% 
+#'  group_by(Subject) %>% 
+#' summarize(cmax = max(conc)) %>% peek 
+#' cmax_theoph # still saves full output
 peek <- function(df, n = 5, message = NULL) {
   if (!is.null(message)) {
     message(message)
@@ -21,6 +28,8 @@ peek <- function(df, n = 5, message = NULL) {
   message("Taking a peek...")
   } 
   print(head(df, n))
+  message("...................")
+  print(tail(df, n))
   return(df)
 }
 
