8eacd2a15ad89b14921e2c36ab72a875ab2de653
Devin Pastoor <devin.pastoor@gmail.com>
2015-09-02 17:44:50 -0400

    update nca documentation per #25

--- R/NCA.R
+++ R/NCA.R
@@ -11,7 +11,7 @@
 #' \dontrun{
 #' library(PKPDdatasets)
 #' sd_oral_richpk %>% group_by(ID) %>%
-#' do(data.frame(NCA(.$Time, .$Conc, .$Dose)))
+#' do(data.frame(nca(.$Time, .$Conc, .$Dose)))
 #' }
 #' @export
 nca <-function(.time, 
