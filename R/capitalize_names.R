8cd4816befb7a73793aa1f6fd18faa34f9f17a5b
Devin Pastoor <devin.pastoor@gmail.com>
2016-04-21 08:45:15 -0400

    add lowercase_names and bump version

--- R/capitalize_names.R
+++ R/capitalize_names.R
@@ -14,3 +14,22 @@ capitalize_names <- function(df) {
   names(df) <- toupper(names(df))
   return(df)
 }
+
+#' lowercase all names for a dataframe
+#' @param df data frame to lowercase names
+#' @details
+#' is a simple wrapper function to reduce typing and more easily pass data
+#' as it is read from a file
+#' @examples 
+#' \dontrun{
+#' df <- lowercase_names(df)
+#' # make sure all names are lowered as a file is read
+#' df <- lowercase_names(read.csv(...))
+#' # or via dplyr pipe syntax
+#' df <- read.csv(...) %>% lowercase_names
+#' }
+#' @export
+lowercase_names <- function(df) {
+  names(df) <- tolower(names(df))
+  return(df)
+}
