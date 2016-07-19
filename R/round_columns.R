72bdbcd93705206537be619315b28beb1d91b49e
Devin Pastoor <devin.pastoor@gmail.com>
2016-07-18 21:17:28 -0400

    refactor round_columns to pseudo tuples to specify groups of columns to round

--- R/round_columns.R
+++ R/round_columns.R
@@ -1,23 +1,39 @@
 #' round columns
 #' @param df data frame
-#' @param col_list list of columns and number of digits to round
+#' @param col_list list of vectors of columns and number of digits to round each
+#' @details 
+#' col_list should contain a list of list tuples, where the first element
+#' is a vector of column names to round, and the second value should be the number
+#' of digits to round to
 #' @examples
 #' \dontrun{
 #' library(PKPDdatasets)
-#' col_list <- list(list("AGE", 1), 
-#' list("WEIGHT", 2),
-#' list("SCREATININE", 2),
-#' list("SERUMALT", 2),
-#' list("DELDBP", 2),
-#' list("DELSBP", 2))
-#' round_columns(aht_trial2, col_list)
+#' col_list <- list(
+#' list("AGE", 1), 
+#' list(c("WEIGHT",
+#'        "SCREATININE",
+#'        "SERUMALT", 
+#'        "DELDBP",
+#'        "DELSBP"),
+#'      2)
+#'  )
+#' aht_trial_rounded <- round_columns(aht_trial2, col_list)
 #' }
 #' @export
 round_columns <- function(df, col_list) {
-  df <- df
-  for (i in seq_along(rounded)) {
-    df[[col_list[i][[1]][[1]]]] <- round(df[[col_list[i][[1]][[1]]]], 
-                                                col_list[i][[1]][[2]])
-  }
+  lapply(col_list, function(list_tuple) {
+    for (col in list_tuple[[1]])  {
+      df[[col]] <<- round(df[[col]], list_tuple[[2]])
+    }
+  })
   return(df)
 }
+col_list <- list(
+list("WEIGHT", 1),
+list(c("SCREATININE",
+       "SERUMALT",
+       "DELDBP",
+       "DELSBP"),
+     2)
+ )
+aht_trial_rounded <- round_columns(aht_trial2, col_list)
