6a66b9a02b9209efcc0df78426df110a8af3cb27
Devin Pastoor <devin.pastoor@gmail.com>
2016-07-18 21:23:59 -0400

    add some error checking

--- R/round_columns.R
+++ R/round_columns.R
@@ -21,19 +21,19 @@
 #' }
 #' @export
 round_columns <- function(df, col_list) {
-  lapply(col_list, function(list_tuple) {
-    for (col in list_tuple[[1]])  {
-      df[[col]] <<- round(df[[col]], list_tuple[[2]])
+  round_col_group <- function(list_tuple) {
+    if (!is.numeric(list_tuple[[2]])) {
+      stop(paste("Rounding value must be numeric, instead of provided value of:", list_tuple[[2]]))
     }
-  })
+    for (col in list_tuple[[1]])  {
+      if (col %in% names(df)) {
+        df[[col]] <<- round(df[[col]], list_tuple[[2]])
+      } else {
+        warning(paste("No column with name:", col, "detected"))
+      }
+    }
+  }
+  lapply(col_list, round_col_group)
   return(df)
 }
-col_list <- list(
-list("WEIGHT", 1),
-list(c("SCREATININE",
-       "SERUMALT",
-       "DELDBP",
-       "DELSBP"),
-     2)
- )
-aht_trial_rounded <- round_columns(aht_trial2, col_list)
+
