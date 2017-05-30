6508c1d7f4ff8e49a97eb8d7750bcc3b6fda1512
Devin Pastoor <devin.pastoor@gmail.com>
2017-05-30 08:23:49 -0400

    has_ext tests

--- R/has_ext.R
+++ R/has_ext.R
@@ -20,11 +20,3 @@ has_ext <- function(name, ext, match_case=TRUE) {
         perl=T) 
 }
 
-# TESTS TO MOVE INTO PROPER TESTING FOLDER
-# test_that("has_ext properly matches extensions for possible scenarios", {
-# expect_true(has_ext("test.rmd", ".rmd"))
-# expect_true(has_ext("test.Rmd", ".rmd", match_case=F))
-# expect_false(has_ext("testrmd", ".rmd"))
-# expect_false(has_ext("test.rmd2", ".rmd"))
-# })
-
