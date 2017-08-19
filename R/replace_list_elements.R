2c23ceb4207b15119ed91b26ffa5a9143822e068
Devin Pastoor <devin.pastoor@gmail.com>
2017-08-19 11:36:26 -0400

    update replace list elements

--- R/replace_list_elements.R
+++ R/replace_list_elements.R
@@ -1,22 +1,23 @@
 #' replace list elements by name
-#' @param list original list
+#' @param .list original list
 #' @param replacement replacement list
+#' @param add add replacement values if they don't exist, default = TRUE
 #' @details
 #' Finds and replaces list elements by name and throws an error if an 
 #'    element is not available in the original list.
 #' @examples 
-#' \dontrun{
-#' list <- list(dv = "dv", idv = "idv")
-#' replacement <- list(dv = "conc", idv = "time")
-#' list <- replace_list_elements(list, replacement)
-#' }
+#' olist <- list(dv = "dv", idv = "idv", notreplace = "somethingelse")
+#' replacement <- list(dv = "conc")
+#' combined_list <- replace_list_elements(olist, replacement)
 #' @export
-replace_list_elements <- function(list, replacement) {
-  missing <- which(!names(replacement) %in% names(list))
+replace_list_elements <- function(.list, replacement, add = TRUE) {
+  missing <- which(!names(replacement) %in% names(.list))
   if(length(missing) != 0) {
-    warning(paste("Nothing named: ", paste(names(replacement)[missing], collapse= ", ", "found to replace") ))
-    replacement <- replacement[-missing]
+    if (add) {
+      warning(paste("Nothing named: ", paste(names(replacement)[missing], collapse= ", ", "found to replace")))
+    } else {
+      stop(paste("Nothing named: ", paste(names(replacement)[missing], collapse= ", ", "found to replace")))
+    }
   }
-  list[names(replacement)] <- lapply(names(replacement), function(x) list[[x]] <- replacement[[x]])
-  return(list)
+  return(utils::modifyList(.list, replacement))
 }
