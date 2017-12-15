9173216646e717a35e49a5395f0539c8e671af94
Devin Pastoor <devin.pastoor@gmail.com>
2017-12-15 15:42:51 -0500

    add some examples

--- R/chunking.R
+++ R/chunking.R
@@ -4,6 +4,27 @@
 #' @details
 #' works very well with hadley wickham's purrr package to create a column
 #' to split on then subsequently plot, see \code{vignette("Multiplot")} for details
+#' @examples 
+#' #chunking will provide the chunk index by splitting the data as evenly as possible
+#' # into the number chunks specified
+#' letters[1:9]
+#' 
+#' chunk(letters[1:9], 3)
+#' 
+#' letters[c(1, 1, 1:7)]
+#' chunk(letters[c(1, 1, 1:7)], 3)
+#' 
+#' # sometimes you want to evenly chunk by unique values rather than purely balancing
+#' chunk_grp(c(1, 1, 1:7), 3)
+#' 
+#' # a next step after chunking is splitting into a list, so this does thus for you
+#' 
+#' # chunk list will both split the data and keep the original values
+#' chunk_list(letters[1:9], 3)
+#' 
+#' # in this case ragged arrays will be created to keep the number of 
+#' # unique elements consistent as possible between chunks
+#' chunk_grp_list(c(letters[1], letters[1], letters[1:7]), 3)
 #' @rdname chunk
 #'@export
 ids_per_plot <- function(id, id_per_plot = 9) {
