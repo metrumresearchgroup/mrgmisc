c7819126ee223f2e97036e90c66e83781bb75751
michaelmcd18 <michaelm@metrumrg.com>
2022-08-31 14:37:13 -0400

    update chunk

--- R/chunking.R
+++ R/chunking.R
@@ -1,9 +1,8 @@
-#' split IDs into groups to use for subsequent plotting
-#' @param id vector of ids (eg id column)
-#' @param id_per_plot number of ids per plot. Default to 9
-#' @details
-#' works very well with hadley wickham's purrr package to create a column
-#' to split on then subsequently plot, see \code{vignette("Multiplot")} for details
+
+#' Chunking inputs...
+#' 
+#' chunk by group, unique values, and return as a vector or a list with elememts
+#' 
 #' @examples 
 #' #chunking will provide the chunk index by splitting the data as evenly as possible
 #' # into the number chunks specified
@@ -27,7 +26,21 @@
 #' # in this case ragged arrays will be created to keep the number of 
 #' # unique elements consistent as possible between chunks
 #' chunk_grp_list(c(letters[1], letters[1], letters[1:7]), 3)
-#' @rdname chunk
+#' @param .x vector of values
+#' @param .nchunk number of chunks to identify
+#' @export
+chunk <- function(.x, .nchunk = parallel::detectCores()) {
+  mod <- length(.x)%/%.nchunk
+  bin_number <- sort(rep(1:.nchunk, each = mod, length.out = length(.x)))
+  return(bin_number)
+}
+
+
+#' @describeIn chunk split IDs into groups to use for subsequent plotting
+#' works very well with hadley wickham's purrr package to create a column
+#' to split on then subsequently plot, see \code{vignette("Multiplot")} for details
+#' @param id vector of ids (eg id column)
+#' @param id_per_plot number of ids per plot. Default to 9
 #'@export
 ids_per_plot <- function(id, id_per_plot = 9) {
   if(!is.vector(id)) {
@@ -43,20 +56,9 @@ ids_per_plot <- function(id, id_per_plot = 9) {
   bin_number[match(id, uid)]
 }
 
-#' chunk
-#' @param .x vector of values
-#' @param .nchunk number of chunks to identify
-#' @rdname chunk
-#' @details 
-#' chunk by group, unique values, and return as a vector or a list with elememts
-#' @export
-chunk <- function(.x, .nchunk = parallel::detectCores()) {
-  mod <- length(.x)%/%.nchunk
-  bin_number <- sort(rep(1:.nchunk, each = mod, length.out = length(.x)))
-  return(bin_number)
-}
 
-#' @rdname chunk
+
+#' @describeIn chunk _this one is for groups, it returns  this..._
 #' @export
 chunk_grp <- function(.x, .nchunk = parallel::detectCores()) {
   .c <- chunk(unique(.x), .nchunk)
