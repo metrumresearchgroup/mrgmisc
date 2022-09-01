cc4264e448fdd5a6eb5ca6b8050978856288cd0f
michaelmcd18 <michaelm@metrumrg.com>
2022-09-01 10:05:32 -0400

    rearrange functions

--- R/replace_empty.R
+++ R/replace_empty.R
@@ -11,18 +11,3 @@ replace_empty <- function(x) {
   x[x == ""] <- NA
   x
 }
-#' Convert '.' values into missing values.
-#'
-#' @param x A character vector
-#' @return A character vector with '.' replaced by NA values.
-#' @details
-#' good for nonmem preprocessing as missing values are represented with '.' in NONMEM
-#' @export
-#' @examples
-#' x <- c(".", "1", "1")
-#' replace_dots(x)
-replace_dots <- function(x) {
-  stopifnot(is.character(x))
-  x[x == "."] <- NA
-  x
-}
