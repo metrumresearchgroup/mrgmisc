2733765d4800e99a18d53e3c43573318789d98de
michaelmcd18 <michaelm@metrumrg.com>
2022-09-12 14:30:40 -0400

    update resampling tests

--- tests/testthat/test-resampling.R
+++ tests/testthat/test-resampling.R
@@ -17,8 +17,8 @@ test_that("resample df properly recombines when it needs to make extra draws [MR
 })
 
 test_that("Appropriate error occurs", {
-  expect_error(resample_df(dapa, key_cols = "ID", strat_cols = "AGE", key_col_name = "Key", n=0), 
-               "n should be 1 or greater")
+  expect_error(resample_df(Theoph, key_cols = "Subject", strat_cols = 2,key_col_name = "Key", n=20),
+               "To set the number of samples please explicitly specify 'n = <num>'")
 })
 
 test_that("Resampling works with no stratification", {
@@ -31,4 +31,9 @@ test_that("Resampling works with no replacement", {
   df1 <- resample_df(Theoph, key_cols = "Subject", key_col_name = "Key", n=5, replace = FALSE)
   expect_equal(nrow(df1), 55)
   expect_equal(dplyr::n_distinct(df1$Key), 5)
+})
+
+test_that("works with n as Null", {
+  df2 <- resample_df(Theoph, key_cols = "Subject")
+  expect_equal(nrow(df2), 132)
 })
