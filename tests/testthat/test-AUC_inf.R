75990d1dd55e5e802b89ea206193f21e77b59b5b
michaelmcd18 <michaelm@metrumrg.com>
2022-09-12 13:52:42 -0400

    add AUC inf tests

--- tests/testthat/test-AUC_inf.R
+++ tests/testthat/test-AUC_inf.R
@@ -11,3 +11,22 @@ test_that("Remove NA works", {
   expect_equal(auc_inf(df1$Time, df1$conc, na.rm = FALSE), setNames(NA_real_, "AUC0_inf"))
   expect_equal(suppressWarnings(auc_inf(df1$Time, df1$conc, na.rm=TRUE)), setNames(1060.009 - 0.000468, "AUC0_inf"))
 })
+
+test_that("Correct output if all input NA", {
+  df2 <- Theoph
+  df2$conc <- NA_real_
+  expect_equal(auc_inf(df2$Time, df2$conc), setNames(NA, "AUC0_inf"))
+})
+
+test_that("Correct output if all DV is 0", {
+  df2 <- Theoph
+  df2$conc <- 0
+  expect_equal(auc_inf(df2$Time, df2$conc), setNames(0, "AUC0_inf"))
+})
+
+test_that("Correct output if all time is 0", {
+  df2 <- Theoph
+  df2$Time <- 0
+  expect_error(auc_inf(df2$Time, df2$conc), "lambda_z is NA")
+})
+
