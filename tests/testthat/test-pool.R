2db93d9e70a63dca838850ea60d7d33203e08349
michaelmcd18 <michaelm@metrumrg.com>
2022-09-12 15:09:49 -0400

    update pool tests

---
+++ tests/testthat/test-pool.R
@@ -0,0 +1,6 @@
+test_that("Appropriate groupings made from two vectors with similarities", {
+  tempPOOL <- pool(letters[1:5], letters[4:8])
+  expect_equal(tempPOOL$x, c("a", "b", "c"))
+  expect_equal(tempPOOL$y, c("f", "g", "h"))
+  expect_equal(tempPOOL$both, c("d", "e"))
+})
