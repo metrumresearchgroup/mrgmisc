0d2c73fcb2ca8f545d0221d97c231476489be928
michaelmcd18 <michaelm@metrumrg.com>
2022-09-13 17:23:19 -0400

    requirements and stories update

--- tests/testthat/test-s_quanties.R
+++ tests/testthat/test-s_quanties.R
@@ -1,15 +1,15 @@
-test_that("s_quantiles works as anticipated", {
+test_that("s_quantiles works as anticipated [MRG-SQUN-001]", {
   df1 <- sd_oral_richpk %>% dplyr::group_by(Gender, Time) %>% s_quantiles(Conc, c(0.05, 0.5, 0.95))
   expect_equal(df1$Conc_q5[2], setNames(8.266324 + 4.73e-07, "5%"))
   expect_equal(df1$Conc_q95[6], setNames(95.82737, "95%"))
 })
 
-test_that("Error occurs with inappropriate input", {
+test_that("Error occurs with inappropriate input [MRG-SQUN-002]", {
   expect_error(sd_oral_richpk %>% dplyr::group_by(Gender, Time) %>% s_quantiles(Conc, c(0.05, 0.5, 1.95)))
   expect_error(sd_oral_richpk %>% dplyr::group_by(Gender, Time) %>% s_quantiles(Conc, c(-0.05, 0.5, 1.95)))
 })
 
-test_that("Works with variety of quantile inputs", {
+test_that("Works with variety of quantile inputs [MRG-SQUN-001]", {
   df1 <- sd_oral_richpk %>% dplyr::group_by(Gender, Time) %>% s_quantiles(Conc, c(0.05, 0.5, 0.6, 0.7, 0.95))
   df2 <- sd_oral_richpk %>% dplyr::group_by(Gender, Time) %>% s_quantiles(Conc, c(0.05))
   expect_true(class(df1$Conc_q70) == "numeric")
--- tests/testthat/test-snap.R
+++ tests/testthat/test-snap.R
@@ -1,18 +1,18 @@
-test_that("works with different type input vectors", {
+test_that("works with different type input vectors [MRG-SNAP-001]", {
   expect_equal(snap(c(0.0, 1.0, 1.2, 2.0, 2.9, 3)), c(0, 1, 1, 2, 3, 3))
   expect_error(snap(c("0.0", "1.0", "1.2", "2.0", "2.9", "3")))
   
   expect_equal(snap(-3:3, 0.3), c(-3.0, -2.1, -0.9, 0.0, 0.9, 2.1, 3.0))
 })
 
-test_that("works with non-unqiue elements in c", {
+test_that("works with non-unqiue elements in c [MRG-SNAP-001]", {
   expect_equal(snap(c(1, 1, 1, 2, 3), 0.3), c(0.9, 0.9, 0.9, 2.1, 3.0))
 })
 
-test_that("works with NA", {
+test_that("works with NA [MRG-SNAP-001]", {
   expect_equal(snap(c(3,NA,5), c(2,3,6)), c(3, NA, 6))
 })
 
-test_that("returns all NA if rule is 0", {
+test_that("returns all NA if rule is 0 [MRG-SNAP-001]", {
   expect_true(is.na(all(snap(c(3,NA,5), numeric(0)))))
 })
