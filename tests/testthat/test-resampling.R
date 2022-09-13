d249594cdd05d342bf0ed51da69d20d0b7bbe493
michaelmcd18 <michaelm@metrumrg.com>
2022-09-13 17:03:18 -0400

    update stories and requirements

--- tests/testthat/test-resampling.R
+++ tests/testthat/test-resampling.R
@@ -9,31 +9,31 @@ no_extras <- resample_df(dapa, key_cols = "ID", strat_cols = "AGE", key_col_name
 has_extras <- resample_df(dapa, key_cols = "ID", strat_cols = "AGE", key_col_name = "Key", n=10)
 
 
-test_that("resample df properly recombines when it needs to make extra draws [MRG-MISC-009]", {
+test_that("resample df properly recombines when it needs to make extra draws [MRG-RESM-001]", {
   expect_equal(nrow(no_extras), 10)
   expect_equal(dplyr::n_distinct(no_extras$Key), 5)
   expect_equal(nrow(has_extras), 20)
   expect_equal(dplyr::n_distinct(has_extras$Key), 10)
 })
 
-test_that("Appropriate error occurs", {
+test_that("Appropriate error occurs [MRG-RESM-003]", {
   expect_error(resample_df(Theoph, key_cols = "Subject", strat_cols = 2,key_col_name = "Key", n=20),
                "To set the number of samples please explicitly specify 'n = <num>'")
 })
 
-test_that("Resampling works with no stratification", {
+test_that("Resampling works with no stratification [MRG-RESM-002]", {
   df1 <- resample_df(Theoph, key_cols = "Subject", key_col_name = "Key", n=20)
   expect_equal(nrow(df1), 220)
   expect_equal(dplyr::n_distinct(df1$Key), 20)
 })
 
-test_that("Resampling works with no replacement", {
+test_that("Resampling works with no replacement [MRG-RESM-002]", {
   df1 <- resample_df(Theoph, key_cols = "Subject", key_col_name = "Key", n=5, replace = FALSE)
   expect_equal(nrow(df1), 55)
   expect_equal(dplyr::n_distinct(df1$Key), 5)
 })
 
-test_that("works with n as Null", {
+test_that("works with n as Null [MRG-RESM-003]", {
   df2 <- resample_df(Theoph, key_cols = "Subject")
   expect_equal(nrow(df2), 132)
 })
