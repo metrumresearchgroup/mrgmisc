01a7471a2ebe70a857737bf04e349af77a90134c
michaelmcd18 <michaelm@metrumrg.com>
2022-09-29 11:16:28 -0400

    update requirements

--- tests/testthat/test-as_nmctl.R
+++ tests/testthat/test-as_nmctl.R
@@ -1,22 +1,22 @@
-test_that("read nmctl works as anticipated", {
+test_that("read.nmctl expected performance: Able to read control file [MRG-ASNM-001]", {
   ctl_z <- read.nmctl(system.file("extdata/exam_ctl.ctl", package = "mrgmisc"))
   expect_equal(ctl_z$prob, "RUN# 100 - fit of Phase I data base model")
   expect_equal(ctl_z$pk[5], "KA   = EXP(THETA(1)+ETA(1))")
 })
 
-test_that("nmctl writer works", {
+test_that("write.nmctl expected performance: Able to write out control files [MRG-ASNM-002]", {
   ctl_z <- read.nmctl(system.file("extdata/exam_ctl.ctl", package = "mrgmisc"))
   writeTemp <- tempfile(fileext = ".ctl")
   write.nmctl(ctl_z$prob, file = writeTemp)
   expect_true(file.exists(writeTemp))
 })
 
-test_that("as character nmctl reader works", {
+test_that("read.nmctl expected performance: as character nmctl reader works [MRG-ASNM-001]", {
   ctl_z <- read.nmctl(system.file("extdata/exam_ctl.ctl", package = "mrgmisc"))
   expect_equal(as.character.nmctl(ctl_z$omega)[1], " BLOCK(3)")
 })  
 
-test_that("expected output for NULL input to nmctl character", {
+test_that("read.nmctl expected performance: expected output for NULL input to nmctl character [MRG-ASNM-001]", {
   expect_equal(as.character.nmctl(NULL), character(0))
 })
 
