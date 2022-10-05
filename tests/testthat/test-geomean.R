89aab64c37452ab71ea8d7b708134db47f856591
michaelmcd18 <michaelm@metrumrg.com>
2022-10-05 14:53:22 -0400

    update requirements and stories for right & geomean

--- tests/testthat/test-geomean.R
+++ tests/testthat/test-geomean.R
@@ -1,4 +1,4 @@
-test_that("geomean calculates accurate geometric mean", {
+test_that("geomean calculates accurate geometric mean [MRG-GEOM-001]", {
   expect_equal(geomean(Theoph$Wt), 68.98671 - 4.24e-06)
   expect_equal(geomean(c(1, 2, 3, 4, 5)), 2.605171 + 8.47e-08)
 })
--- tests/testthat/test-right.R
+++ tests/testthat/test-right.R
@@ -1,4 +1,4 @@
-test_that("right grabs the appropriate amount of characters from the end of the string", {
+test_that("right grabs the appropriate amount of characters from the end of the string [MRG-RGHT-001]", {
   expect_equal(right("string-123", 3), "123")
   expect_equal(right("string-123", 1), "3")
   expect_equal(right("string-123", 0), "")
