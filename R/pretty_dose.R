7a7f77d299e6d521e7da2a9f5b73b2e64c08134c
michaelmcd18 <michaelm@metrumrg.com>
2022-10-11 16:22:49 -0400

    add pretty_dose tests & requirements

--- R/pretty_dose.R
+++ R/pretty_dose.R
@@ -1,14 +1,19 @@
-#' function to overwrite DOSE column into factor and ordered by value
+#' Overwrite DOSE column into factor and ordered by value
 #'
 #' @param .dat data.frame with column `.dosecol`
 #' @param .dosecol quoted name of column containing dosing information
 #' @param .units unit to be appended to output dose column
 #' 
+#' @examples 
+#' 
+#' dose_fix <- pretty_dose(Theoph, .dosecol = "Dose", .units = "mg")
+#' dose_fix$Dose
+#' 
 #' @export
 pretty_dose <- function(.dat, .dosecol="DOSE", .units="mg"){
   .dat$TMP <- .dat[[.dosecol]]
   .dat %>%
-    mutate(Dose = factor(
+    dplyr::mutate(Dose = factor(
       ifelse(TMP == 0, "Placebo", paste(TMP, .units))) %>% reorder(TMP)
-    ) %>% select(-TMP)
+    ) %>% dplyr::select(-TMP)
 }
