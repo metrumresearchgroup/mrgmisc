e09bd4577f09dbdf9825e724b30fa8b7189cdec5
michaelmcd18 <michaelm@metrumrg.com>
2022-10-12 15:19:11 -0400

    add nsub tests & requirements

--- R/nsub.R
+++ R/nsub.R
@@ -1,7 +1,11 @@
-#' Count IDs
+#' Count number of IDs in a NONMEM data set
 #' 
 #' @param .df data.frame containing column .id
 #' @param .id column name that will be used to uniquely identify IDs
 #' 
+#' @examples 
+#' 
+#' nsub(.df = Theoph, .id = Subject)
+#' 
 #' @export
-nsub <- function(.df, .id=USUBJID) {.df %>% distinct({{ .id }}) %>% nrow()}
+nsub <- function(.df, .id="USUBJID") {.df %>% dplyr::distinct({{ .id }}) %>% nrow()}
