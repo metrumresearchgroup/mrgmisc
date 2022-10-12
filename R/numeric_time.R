7f4fa77493ef79629cc7a506e3f446fb38249cd8
michaelmcd18 <michaelm@metrumrg.com>
2022-10-12 16:12:47 -0400

    add tests and requirements for numeric_time

--- R/numeric_time.R
+++ R/numeric_time.R
@@ -3,6 +3,14 @@
 #' @param .col column of type character or POSIXct to extract clocktimes from 
 #' to convert to decimal values
 #' 
+#' @examples 
+#' 
+#' timedf <- dplyr::tibble(ID = c(1, 1, 2),
+#' DATETIME = c("2022-01-23T12:34:00", "2022-01-28T04:32:00", "2022-01-21T18:00:00"))
+#'
+#' times <- lubridate::ymd_hms(timedf$DATETIME)
+#' numeric_time(times)
+#'
 #' @export
 numeric_time <- function(.col) {
   ## if DATETIME format, strip clocktime to character
