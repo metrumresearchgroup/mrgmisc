20f4440892bd0344ff6e219ea1d8bbf7d7f7bb4f
michaelmcd18 <michaelm@metrumrg.com>
2022-10-13 15:54:47 -0400

    add requirements and stories for half_life

--- tests/testthat/test-half_life.R
+++ tests/testthat/test-half_life.R
@@ -1,4 +1,4 @@
-test_that("half_life accurately calculates the half life", {
+test_that("half_life accurately calculates the half life [MRG-HALF-001]", {
   expect_equal(half_life(1, 51, 13, 1), 46.51592 + 2.69e-06)
   expect_equal(half_life(12, 0, 0, 13), NaN)
   expect_equal(half_life(0, 1, 2, 0), Inf)
