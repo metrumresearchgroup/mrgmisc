2ef3a2464541b9dbc3a1f21616ef1d1069a6b370
michaelmcd18 <michaelm@metrumrg.com>
2023-05-25 11:07:25 -0400

    Update pool and pool_df to include name of dataframes in comparison

--- tests/testthat/test-pool.R
+++ tests/testthat/test-pool.R
@@ -1,6 +1,6 @@
 test_that("pool creates appropriate groupings from two vectors with similarities [MRG-POOL-001]", {
   tempPOOL <- pool(letters[1:5], letters[4:8])
-  expect_equal(tempPOOL$x, c("a", "b", "c"))
-  expect_equal(tempPOOL$y, c("f", "g", "h"))
+  expect_equal(tempPOOL$`letters[1:5]`, c("a", "b", "c"))
+  expect_equal(tempPOOL$`letters[4:8]`, c("f", "g", "h"))
   expect_equal(tempPOOL$both, c("d", "e"))
 })
