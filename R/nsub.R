4735e0422eb18b296946900ef2b52a3c88268077
michaelm <michaelm@metrumrg.com>
2023-06-15 15:22:17 -0400

    Update nsub

--- R/nsub.R
+++ R/nsub.R
@@ -1,17 +1,38 @@
 #' Count number of IDs in a NONMEM data set
 #' 
-#' @param .df data.frame containing column .id
-#' @param .id column name that will be used to uniquely identify IDs
+#' @param .df data.frame containing subject column
+#' @param .subject_col column name that will be used to uniquely identify IDs
 #' 
 #' @examples 
 #' 
-#' nsub(.df = Theoph, .id = Subject)
+#' nsub(.df = Theoph, .subject_col = "Subject")
 #' 
 #' @author Samuel P Callisto, PhD
 #' 
 #' @export
-nsub <- function(.df, .id) {
+nsub <- function(.df, .subject_col = NULL) {
   
-  .df %>% dplyr::distinct({{ .id }}) %>% nrow()
+  if (!is.null(.subject_col) & !inherits(.subject_col, "character")) {
+    stop(".subject_col must be character format")
+  }
   
+  if (is.null(.subject_col)) {
+    
+    if (!is.null(.df[["ID"]])) {
+      .subject_col = "ID"
+    }
+    
+    if (!is.null(.df[["USUBJID"]])) {
+      .subject_col = "USUBJID"
+    }
+    
+    if (is.null(.subject_col)) {
+      stop("Please define .subject_col")
+    }
+    
+  }
+  
+  .df %>% 
+    dplyr::distinct(!!dplyr::sym(.subject_col)) %>% 
+    nrow()
   }
