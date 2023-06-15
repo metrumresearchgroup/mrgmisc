f55a136a8348bee33a855487cf820bef25ff9254
michaelm <michaelm@metrumrg.com>
2023-06-15 15:37:03 -0400

    Update nsub tests

--- tests/testthat/test-nsub.R
+++ tests/testthat/test-nsub.R
@@ -1,6 +1,18 @@
 test_that("nsub accurately determines the number of patients in a data set [MRG-NSUB-001]", {
-  expect_equal(nsub(.df = Theoph, .id = Subject), 12)
+  expect_equal(nsub(.df = Theoph, .subject_col = "Subject"), 12)
   
-  charID <- Theoph %>% dplyr::mutate(NEWCOL = as.character(Subject))
-  expect_equal(nsub(.df = charID, .id = NEWCOL), 12)
+  charID <- Theoph %>% dplyr::mutate(USUBJID = as.character(Subject))
+  expect_equal(nsub(.df = charID), 12)
+})
+
+test_that("nsub will prevent user from inputting a non-character argument to .subject_col", {
+  
+  expect_error(nsub(.df = Theoph, .subject_col = 1), ".subject_col must be character format")
+  
+})
+
+test_that("nsub will provide number of subjects whether column is numeric or character", {
+  
+  expect_equal(nsub(mtcars, .subject_col = "cyl"), 3)
+  
 })
